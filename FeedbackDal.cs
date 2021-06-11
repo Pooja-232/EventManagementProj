34738243949c03f59f8fb366df6ebd9a8f79a7d0
Pooja-232 <85270465+Pooja-232@users.noreply.github.com>
2021-06-11 19:43:02 +0530

    Add files via upload

---
+++ FeedbackDal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BALForEventMgt;
+
+namespace DALForEventMgt
+{ 
+
+    public class FeedbackDal
+{
+    public void authfeed(Feedback f)
+    {
+        Feedback f1 = new Feedback();
+        SqlConnection cn = new SqlConnection("server=DESKTOP-JO0FKH7\\SQLEXPRESS;Integrated Security=true;database=EventManagement");
+        SqlCommand cmd = new SqlCommand("insert into Feedback values(" + f.P_feedback + ",'" + f.P_name + " ')", cn);
+        cn.Open();
+
+
+        cmd.ExecuteNonQuery();
+
+        cn.Close();
+        
+    }
+}
+}
