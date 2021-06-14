6c9f4b992d7cdc5f80a56ab20f5cc2d7f07d679c
Pooja-232 <85270465+Pooja-232@users.noreply.github.com>
2021-06-14 14:10:30 +0530

    Add files via upload

---
+++ update-speaker.component.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ApicallService } from 'src/app/apicall.service';
+
+
+@Component({
+  selector: 'app-update-speaker',
+  templateUrl: './update-speaker.component.html',
+  styleUrls: ['./update-speaker.component.css']
+})
+export class UpdateSpeakerComponent implements OnInit {
+
+  speakerid:number=0;
+  data:any=[]
+  constructor(private http : HttpClient,private apicallService:ApicallService) { }
+  speakerid:number=0;
+  speakername:string="";
+  password:string="";
+  skills:string="";
+  contactnumber:string="";
+
+  ngOnInit(): void {
+  }
+  search()
+  {
+    
+    //this.suppID=event.suppID;
+    var speaker=new Speaker();
+
+    this.apicallService.getbyid(this.speakerid).subscribe(data => {
+      this.data = data;
+    console.log(this.data);
+    this.speakerid=this.data.speakerid;
+    this.speakername=this.data.speakername;
+    this.password=this.data.password;
+    this.skills=this.data.skills;
+    this.contactnumber=this.data.contactnumber;
+    
+      // console.log(this.supplierdata);
+    });
+
+}
+
+savechanges(event:any)
+{
+  var sp=new Speaker();
+  sp.speakerid=event.speakerid;
+  sp.speakername=event.speakername;
+  sp.password=event.password;
+  sp.skills=event.skills;
+  sp.Contactnumber=event.contactnumber
+  
+// alert(supplier.SupplierID);
+// alert(supplier.ContactName);
+// alert(supplier.CompanyName);
+
+this.apicallService.putspeakerData(this.speakerid,sp);
+
+
+}
+}
